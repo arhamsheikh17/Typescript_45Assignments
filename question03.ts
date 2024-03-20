92d1266139b07c80d939e1acbd7de0f2393f17f0
Arham sheikh <arhamsheikh2250@gmail.com>
2024-03-20 11:43:43 -0700

    typescript assignment

---
+++ question03.ts
@@ -0,0 +1,10 @@
+ // Name Cases: Store a person’s name in a variable,
+//  and then print that person’s name in lowercase, uppercase, and titlecase.
+// Program written by NOUASHAD ALAM on TYPESCRIPT
+
+
+let personsName: string = "arham ul sheikh";
+
+console.log("Lowercase: ", personsName.toLowerCase());
+console.log("Uppercase: ", personsName.toUpperCase());
+
