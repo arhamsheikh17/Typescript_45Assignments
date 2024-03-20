c28665fcb8df48e8455906c593c63f1e45eca494
Arham sheikh <arhamsheikh2250@gmail.com>
2024-03-20 12:30:13 -0700

    typescript assignment

---
+++ question10.ts
@@ -0,0 +1,7 @@
+// Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+
+// my name is arham
+// it is 2024
+// the below output shows name and year
+console.log("Hello arham you are in 2024");
